8d07f0fbba062575cc047de140dbb4afd3a2cb7a
sirben181 <bensonkamauben@gmail.com>
2021-10-18 18:37:46 +0300

    toggling the active class

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const Home=document.querySelector('.navlist')
+const links=document.querySelectorAll('.navlist')
 
-Home.addEventListener("click",()=>{
-   Home.classList.toggle('active')
-})
-Home.addEventListener('hover',()=>{
-    Home.classList.toggle('show')
-})
+links.forEach((link=>{
+    link.addEventListener("click",()=>{
+        if(link.classList.contains('active')){link.classList.remove('active')}
+        else{ 
+        link.classList.add('active') }},{once:false})
+}))
+
+// when i click on one icon it should become active and then remove other active icons
