084a30c119b3dd8748a4f26763204345f1bd92d0
sirben181 <bensonkamauben@gmail.com>
2021-10-18 23:09:51 +0300

    more advanced

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const links=document.querySelectorAll('.navlist')
 
 links.forEach((link=>{
     link.addEventListener("click",()=>{
+        
         if(link.classList.contains('active')){link.classList.remove('active')}
         else{ 
-        link.classList.add('active') }},{once:false})
+        link.classList.add('active') }})
 }))
 
 // when i click on one icon it should become active and then remove other active icons
