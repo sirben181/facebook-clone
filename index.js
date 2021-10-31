2eb69a94c5a9110df1c9fcc16afdf56661e8df7b
sirben181 <bensonkamauben@gmail.com>
2021-10-31 09:13:42 +0300

    amazing

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const path=require('path')
+
+dotenv.config({path:'./config/config.env'});
+
+const PORT=process.env.PORT || 5000
+
 
 const app=express();
 
-const PORT=process.env.PORT || 5000
+
 //adding static folder
 app.use(express.static('public'));
 
